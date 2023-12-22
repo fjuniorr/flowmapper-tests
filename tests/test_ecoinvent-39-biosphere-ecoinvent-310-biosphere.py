7ab5dd7bb7e29e07d9a0b3323e57bad755107d88
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-12-21 22:23:29 -0300

    commit https://github.com/fjuniorr/flowmapper/commit/1ed68040855c0f16e137100a9c570c2ef1154c19

--- tests/test_ecoinvent-39-biosphere-ecoinvent-310-biosphere.py
+++ tests/test_ecoinvent-39-biosphere-ecoinvent-310-biosphere.py
@@ -9,7 +9,7 @@ def test_flowmap(snapshot_json):
     target_flows_path = 'data/ecoinvent-3.10-biosphere.json'
     
     fields = read_field_mapping(field_mapping)
-    transformations = read_migration_files('config/sp-formatted.json')
+    transformations = read_migration_files('config/ei3.9-ei3.10.json')
     
     source_flows = read_flowlist(source_flows_path)
     source_flows = [Flow(flow, fields['source'], transformations) for flow in source_flows]
